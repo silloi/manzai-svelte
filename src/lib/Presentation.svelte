daa21dc644b8661606c96a6f48eb6ed33736739b
silloi <silloi1993@gmail.com>
2022-03-13 16:01:40 +0900

    feature: create Presentation component

---
+++ src/lib/Presentation.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import ResetCss from '$lib/ResetCss.svelte';
+	import Slides from '$lib/Slides.svelte';
+	import Textarea from '$lib/Textarea.svelte';
+
+	export let text = '';
+
+	let isFocused = false;
+	const onFocus = () => (isFocused = true);
+	const onBlur = () => (isFocused = false);
+</script>
+
+<ResetCss />
+
+<div class="flex">
+	<div class="flex-item">
+		<Slides bind:text {isFocused} />
+	</div>
+
+	<div class="flex-item">
+		<Textarea bind:text {onFocus} {onBlur} />
+	</div>
+</div>
+
+<style>
+	.flex {
+		display: flex;
+		flex-direction: column;
+		height: 100vh;
+	}
+
+	@media (min-width: 768px) {
+		.flex {
+			flex-direction: row-reverse;
+			justify-content: flex-end;
+		}
+
+		.flex-item {
+			flex: 1;
+		}
+	}
+</style>
--- src/routes/presentation/index.svelte
+++ src/routes/presentation/index.svelte
@@ -1,44 +1,6 @@
 <script lang="ts">
-	import ResetCss from 'svelte-manzai/ResetCss.svelte';
-	import Slides from 'svelte-manzai/Slides.svelte';
-	import Textarea from 'svelte-manzai/Textarea.svelte';
-
+	import Presentation from '$lib/Presentation.svelte';
 	import protagoras from '$lib/samples/protagoras';
-
-	let isFocused = false;
-	const onFocus = () => (isFocused = true);
-	const onBlur = () => (isFocused = false);
-
-	let text = protagoras;
 </script>
 
-<ResetCss />
-
-<div class="flex">
-	<div class="flex-item">
-		<Slides bind:text {isFocused} />
-	</div>
-
-	<div class="flex-item">
-		<Textarea bind:text {onFocus} {onBlur} />
-	</div>
-</div>
-
-<style>
-	.flex {
-		display: flex;
-		flex-direction: column;
-		height: 100vh;
-	}
-
-	@media (min-width: 768px) {
-		.flex {
-			flex-direction: row-reverse;
-			justify-content: flex-end;
-		}
-
-		.flex-item {
-			flex: 1;
-		}
-	}
-</style>
+<Presentation text={protagoras} />
