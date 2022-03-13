08e9124fd467f3de75e12ca82366a3d029012298
silloi <silloi1993@gmail.com>
2022-03-13 15:34:11 +0900

    feature: add sample text

---
+++ src/lib/samples/texts.ts
@@ -0,0 +1,58 @@
+export const protagoras = `---
+title: Protagoras
+description: Protagoras, written by Plato
+actors:
+  - name: Com
+    type: 1
+    avatar: https://upload.wikimedia.org/wikipedia/commons/9/9e/Plato_and_Aristotle.png
+  - name: Soc
+    type: 2
+    avatar: https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Socrate_du_Louvre.jpg/440px-Socrate_du_Louvre.jpg
+---
+
+Com: Where do you come from, Socrates? And yet I need hardly ask the question, for I know that you have been in chase of the fair Alcibiades. I saw the day before yesterday; and he had got a beard like a man-and he is a man, as I may tell you in your ear. But I thought that he was still very charming.
+
+Soc: What of his beard? Are you not of Homer's opinion, who says
+
+Youth is most charming when the beard first appears? And that is now the charm of Alcibiades.
+
+Com: Well, and how do matters proceed? Have you been visiting him, and was he gracious to you?
+
+Soc: Yes, I thought that he was very gracious; and especially to-day, for I have just come from him, and he has been helping me in an argument. But shall I tell you a strange thing? I paid no attention to him, and several times I quite forgot that he was present.
+
+Com: What is the meaning of this? Has anything happened between you and him? For surely you cannot have discovered a fairer love than he is; certainly not in this city of Athens.
+
+Soc: Yes, much fairer.
+
+Com: What do you mean-a citizen or a foreigner?
+
+Soc: A foreigner.
+
+Com: Of what country?
+
+Soc: Of Abdera.
+
+Com: And is this stranger really in your opinion a fairer love than the son of Cleinias?
+
+Soc: And is not the wiser always the fairer, sweet friend?
+
+Com: But have you really met, Socrates, with some wise one?
+
+Soc: Say rather, with the wisest of all living men, if you are willing to accord that title to Protagoras.
+
+Com: What! Is Protagoras in Athens?
+
+Soc: Yes; he has been here two days.
+
+Com: And do you just come from an interview with him?
+
+Soc: Yes; and I have heard and said many things.
+
+Com: Then, if you have no engagement, suppose that you sit down tell me what passed, and my attendant here shall give up his place to you.
+
+Soc: To be sure; and I shall be grateful to you for listening.
+
+Com: Thank you, too, for telling us.
+
+Soc: That is thank you twice over. Listen then:
+`
